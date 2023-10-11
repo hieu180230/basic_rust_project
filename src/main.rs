a2c1bafdd836bfd2ca3b37f8a29f652759d51083
hieu18022004 <hieu180230@gmail.com>
2023-10-11 17:36:17 +0700

    done audio part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+fn main() -> Result<(), Box<dyn Error>>{
+    let mut audio = Audio::new();
+    audio.add("name", "filename.wav");
+
+    audio.play("name"); //this play audio
+    audio.wait(); //this wait for audio to finish
+    Ok(()) //this return Ok
 }
